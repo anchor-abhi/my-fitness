84c484609daa4b5f3235da60a978ac8115dc80fb
anchor-abhi <theabhishek1802@gmail.com>
2022-08-06 00:43:03 +0530

    hero banner done

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Box, Stack, Typography, Button } from "@mui/material";
+
+import HeroBannerImage from "../assets/images/banner.png";
+
+const HeroBanner = () => {
+  return (
+    <Box
+      sx={{
+        mt: { lg: "212px", xs: "70px" },
+        ml: { sm: "50px" },
+      }}
+      position="relative"
+      p="20px"
+    >
+      <Typography color="#FF2625" fontSize="26px" fontWeight="600">
+        Fitness Club
+      </Typography>
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+        mb="23px" mt="30px"
+      >
+        Sweat, Smile <br /> and Repeat
+      </Typography>
+      <Typography mb={4} fontSize="22px" lineHeight="35px">
+        Check out the most effective exercises
+      </Typography>
+      <Typography fontWeight={600} color="#FF2625" sx={{opacity:0.1, display:{lg:"block", xs:"none"}}} fontSize="200px">Exercise</Typography>
+      <Button sx={{backgroundColor:"#ff2625", padding:"10px"}} variant="contained" color="error" href="#exercises">
+        Explore Exercises
+      </Button>
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
+    </Box>
+  );
+};
+
+export default HeroBanner;
