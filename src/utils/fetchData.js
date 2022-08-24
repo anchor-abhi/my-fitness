c98172c29df9b37e2b4488c82d45f9aed5ec9574
anchor-abhi <theabhishek1802@gmail.com>
2022-08-24 23:53:49 +0530

    api_key updated

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "9367c8d3f8msh5bd8c23bceb3e19p17e65djsnfa6265a34c1a",
+    "X-RapidAPI-Key": "9c1621268emsh1f286a764e6c365p1457c4jsna42dc15d4ecd",
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
@@ -9,7 +9,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "9367c8d3f8msh5bd8c23bceb3e19p17e65djsnfa6265a34c1a",
+    "X-RapidAPI-Key": "9c1621268emsh1f286a764e6c365p1457c4jsna42dc15d4ecd",
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
