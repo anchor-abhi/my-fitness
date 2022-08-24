5e5325a606e29341885f25af33390ca595028a6a
anchor-abhi <theabhishek1802@gmail.com>
2022-08-24 23:59:31 +0530

    api fixed

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -9,7 +9,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "9c1621268emsh1f286a764e6c365p1457c4jsna42dc15d4ecd",
+    "X-RapidAPI-Key": "367c8d3f8msh5bd8c23bceb3e19p17e65djsnfa6265a34c1a",
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
