c19fd767defafe6ef50bf45b9e4940d95b6e1ce4
anchor-abhi <theabhishek1802@gmail.com>
2022-08-25 14:53:06 +0530

    added env for REACT

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,9 +1,7 @@
-import { RAPID_API_KEY } from "../secrets/secret";
-
 export const exerciseOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": RAPID_API_KEY,
+    "X-RapidAPI-Key": `${process.env.REACT_APP_RAPID_API_KEY}`,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
@@ -11,7 +9,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": RAPID_API_KEY,
+    "X-RapidAPI-Key": `${process.env.REACT_APP_RAPID_API_KEY}`,
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
