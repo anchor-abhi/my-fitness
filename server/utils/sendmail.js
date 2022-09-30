10a1a2937435131c46c417ab6abadc18315219d4
anchor-abhi <theabhishek1802@gmail.com>
2022-09-30 15:22:03 +0530

    added env configs

--- server/utils/sendmail.js
+++ server/utils/sendmail.js
@@ -1,35 +1,34 @@
-
+require("dotenv").config;
 const nodemailer = require("nodemailer");
 
-module.exports =function (name, email, message) {
-    let mailTransporter = nodemailer.createTransport({
-      host: "smtp.gmail.com", // hostname
-      service:"gmail",
-      secureConnection: false, // TLS requires secureConnection to be false
-      port: 587, // port for secure SMTP
-      tls: {
-        ciphers: "SSLv3",
-      },
-      auth: {
-        user: "abhiayodhya1802@gmail.com",
-        pass: "hlmymeproscngwnx",
-      },
-    });
-  
-    let mailDetails = {
-      from: "abhiayodhya1802@gmail.com",
-      to: "theabhishek1802@gmail.com",
-      subject: email,
-      text: name+  " sent you message from your PORTFOLIO : "+ message,
-    };
-  
-    mailTransporter.sendMail(mailDetails, function (err, data) {
-      if (err) {
-        console.log(err);
-        console.log("Error Occurs");
-      } else {
-        console.log("Email sent successfully");
-      }
-    });
+module.exports = function (name, email, message) {
+  let mailTransporter = nodemailer.createTransport({
+    host: "smtp.gmail.com", // hostname
+    service: "gmail",
+    secureConnection: false, // TLS requires secureConnection to be false
+    port: 587, // port for secure SMTP
+    tls: {
+      ciphers: "SSLv3",
+    },
+    auth: {
+      user: process.env.gmail_user_auth,
+      pass: process.env.gmail_user_pass,
+    },
+  });
 
-  }
+  let mailDetails = {
+    from: "abhiayodhya1802@gmail.com",
+    to: "theabhishek1802@gmail.com",
+    subject: email,
+    text: name + " sent you message from your PORTFOLIO : " + message,
+  };
+
+  mailTransporter.sendMail(mailDetails, function (err, data) {
+    if (err) {
+      console.log(err);
+      console.log("Error Occurs");
+    } else {
+      console.log("Email sent successfully");
+    }
+  });
+};
